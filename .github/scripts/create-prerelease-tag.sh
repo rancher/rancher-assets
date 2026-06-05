755e97a2706c175cbc20815053df0b132a6d5647
Dan Pock <self@danpock.me>
2026-06-05 18:36:35 -0400

    wip idk

--- .github/scripts/create-prerelease-tag.sh
+++ .github/scripts/create-prerelease-tag.sh
@@ -2,11 +2,12 @@
 set -e
 
 VERSION="$1"
-MAJOR="$2"
-CURRENT_STABLE="$3"
-CURRENT_PRERELEASE="$4"
+COMMIT_SHA="$2"
+MAJOR="$3"
+CURRENT_STABLE="$4"
+CURRENT_PRERELEASE="$5"
 
-git tag -a "$VERSION" -m "Auto pre-release $VERSION
+git tag -a "$VERSION" "$COMMIT_SHA" -m "Auto pre-release $VERSION
 
 Chart Major: $MAJOR
 Triggered by: push to main
